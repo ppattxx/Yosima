e401f9b099e6097a5b7064abe436dea62ecffc04
ppattxx <acahyadava@gmail.com>
2025-04-20 12:18:21 +0700

    3

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -6,36 +6,40 @@ import Smilearn from "/public/images/Smilearn.png";
 import Smilearn2 from "/public/images/Smilearn2.png";
 import SmilearnMob from "/public/images/SmilearnMob.png";
 import SmilearnMob2 from "/public/images/SmilearnMob2.png";
+import Feed1 from "/public/images/feed1.png";
+import Feed2 from "/public/images/feed2.png";
 
 
 export const products = [
   {
-    href: "https://smilearn.agileteknik.com",
+    href: "https://drive.google.com/drive/folders/1yWr7RhDLiMCHsKvrm2Nu8wo_beqlMYFa",
     title: "Social Media Design",
     description:
-      "A design and development studio that focuses on building quality apps.",
-    thumbnail: Smilearn,
-    images: [Smilearn, Smilearn2],
-    stack: ["React.js", "Tailwindcss"],
+      "A social media specialist experienced in managing campaigns, creating engaging content, and growing brand presence across various platforms.",
+    thumbnail: Feed1,
+    images: [Feed1, Feed2],
+    stack: ["Canva", "Photoshop"],
     slug: "Smilearn",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        Social Media Specialist – Fifa Gym
+        I was responsible for managing and improving the gym's online performance on
+        social media platforms. In this role, I focused on content strategy,
+        monitoring social media trends, and consistently delivering up to 10 pieces of
+        content each week.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+        Creative Team – PT. Tiga Pilar Nutrisi
+        In this position, I was responsible for creating content plans and optimizing
+        clients' social media through various marketing strategies. Additionally,
+        I also served as talent in video production conducted by the creative team
         </p>{" "}
+        <p>
+        Digital Marketing – PT. Ori Karya Shanandara
+        I was responsible for managing and improving the company&apos;s online sales performance on digital platforms.
+        In this role, I focused on content strategy, trend monitoring on social media, and utilizing Instagram Ads.
+        </p>
       </div>
     ),
   },
